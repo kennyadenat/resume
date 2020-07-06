05316e998090cd7dbae7eee387ce36c967cc8690
Kehinde Adelugba <kennyadenat09@gmail.com>
2020-07-06 12:28:45 +0100

    updated myresume information

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  {
+    path: '**', pathMatch: 'full', redirectTo: '/error'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
